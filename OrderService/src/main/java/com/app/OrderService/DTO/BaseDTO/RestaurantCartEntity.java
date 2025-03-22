576a31bd5c019092681510eb5c264c4470d0fb7c
letan-165 <2251160057@ut.edu.vn>
2025-03-22 12:06:56 +0700

    update

--- OrderService/src/main/java/com/app/OrderService/DTO/BaseDTO/RestaurantCartEntity.java
+++ OrderService/src/main/java/com/app/OrderService/DTO/BaseDTO/RestaurantCartEntity.java
@@ -2,15 +2,21 @@ package com.app.OrderService.DTO.BaseDTO;
 
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+import lombok.experimental.SuperBuilder;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Data
-@Builder
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class RestaurantCartDTO {
     String restaurantID;
-    List<ItemCartDTO> menu;
+
+    @Builder.Default
+    Map<Long ,ItemCartDTO> menu = new HashMap<>();
 }
